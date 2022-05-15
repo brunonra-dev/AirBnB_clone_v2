bfcac1d2c25b91b6debe22e6369e803eedd22075
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-15 18:42:20 -0300

    fix

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
+        cities = relationship('City', backref='state', cascade='all, delete')
     else:
         name = ""
 
